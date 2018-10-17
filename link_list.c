71ada53a0c6eaa5459c3dafb7c5c1ca7b0190ef1
Kerwin Chen <kchen37@stuy.edu>
2018-10-17 11:00:33 -0400

    edited files

--- link_list.c
+++ link_list.c
@@ -1,13 +1,32 @@
-struct song_node * insert_front(struct song_node *head, struct song_node *insertee){
+struct song_node * insert_front(struct song_node *head, char[] nombre, char[] artista){
+  struct song_node *n;
+  n->name = nombre;
+  n->artist = artista;
   //int x = ((insertee->name)[0]) - 'a';
   //data += x;
-  insertee->next = *data;
-  data = insertee;
-  return *data;
+  n->next = *head;
+  head = n;
+  return *head;
 }
 
 struct song_node * insert_ordered(struct song_node *head, struct song_node *insertee){
   //int x = ((insertee->name)[0]) - 'a';
   //data += x;
-  while(!strcmp(insertee->name,head->name)
+  while(strcmp(insertee->artist, head->artist) > 0 ) {
+    head = head->next;
+  }
+  if(!strcmp(insertee->artist, head->artist)){
+    while(strcmp(insertee->name, head->name) > 0 ) {
+      head = head->next;
+    }
+  }
+  return song_node * insert_front(head, insertee);
+}
+
+int print_list(struct song_node *head){
+  while(head->next){
+    printf("%s : %s |", head->artist, head->name);
+  }
+  return 0;
+}
 
