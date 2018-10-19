48a359016a0c218897afe135d605ac891263b5d7
TahmidJamal <tjamal@stuy.edu>
2018-10-18 23:53:33 -0400

    fjdsljdf

--- music_library.c
+++ music_library.c
@@ -1,3 +1,24 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include "music_library.h"
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "linked_list.h"
+#include "library.h"
+
+void add_song(char *song, char *artist) {
+	int letter = artist[0] - 'a';
+	if (letter >= 27 || letter < 0) {
+		letter = 26;
+	}
+	struct song_node head = data[letter];
+	data[letter] = insert_ordered(head, song, artist);
+}
+
+struct node* find_song(char *song, char *artist) {
+	int letter = artist[0] - 'a';
+	if (letter >= 27 || letter < 0) {
+		letter = 26;
+	}
+	struct song_node head = data[letter];
+	return find_node(head, song, artist);
+}
