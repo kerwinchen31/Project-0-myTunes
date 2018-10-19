4f0a571c534188d822fe2dc99512a3b2477f1bc5
TahmidJamal <tjamal@stuy.edu>
2018-10-18 23:14:32 -0400

    header lib

--- music_library.h
+++ music_library.h
@@ -1,5 +1,27 @@
 #ifndef MUSIC_LIBRARY_H
 #define MUSIC_LIBRARY_H
 
+struct node *table[27];
+
+void add_song(char *song, char *artist);
+
+struct node* find_song(char *song, char *artist);
+
+struct node* find_artist(char *artist);
+
+void print_under_letter(char letter);
+
+void print_under_artist(char *artist);
+
+void print_all();
+
+void shuffle();
+
+void delete_song(char *song, char *artist);
+
+void clear();
+
+#endif
+
 
 #endif 
