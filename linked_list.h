31afd2d354421b4fa6f6b0172f99fbef371a658f
kerwinchen31 <kchen37@stuy.edu>
2018-10-19 00:37:56 -0400

    edited

--- linked_list.h
+++ linked_list.h
@@ -7,17 +7,17 @@ struct song_node {
 	struct song_node *next; 
 };
 
-struct song_node * insert_front(struct song_node *head, char *name, char *song);
+struct song_node * insert_front(struct song_node *head, char *song, char *artist);
 
-struct song_node * insert_ordered(struct song_node *head, char *name, char *song);
+struct song_node * insert_ordered(struct song_node *head, char *song, char *artist);
 
 void print_list(struct song_node *head);
 
-struct song_node *find_node(struct song_node *head, char *name, char *artist);
+struct song_node *find_node(struct song_node *head, char *song, char *artist);
 
 struct song_node * random_node(struct song_node *head);
 
-struct song_node * remove_node(struct song_node *head, char *name, char *artist);
+struct song_node * remove_node(struct song_node *head, char *song, char *artist);
 
 struct song_node * free_list(struct song_node *head);
 
--- main.c
+++ main.c
@@ -3,7 +3,16 @@
 #include "linked_list.h"
 #include "music_library.h"
 
-int main() { 
+int main() {
+  struct song_node *lib[27];
+  int i;
+  char c = 'a';
+  for (i = 0; i < 27; i++){
+    struct song_node *node = NULL;
+    node = insert_front(node, c, c);
+    lib[i] = node;
+    a++;
+  }
 
-
+  
 }
