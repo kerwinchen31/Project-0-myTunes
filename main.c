65ee5765ab1f44420fcfd88b1c79fad0a5798fd6
kerwinchen31 <kchen37@stuy.edu>
2018-10-19 01:50:26 -0400

    added

--- main.c
+++ main.c
@@ -4,15 +4,19 @@
 #include "music_library.h"
 
 int main() {
-  char c;
-  for (c = 'a'; c < 'z'; c++){
-    add_song("hello", "adele");
+  //char c;
+  //for (c = 'a'; c < 'z'; c++){
+  //add_song("hello", "adele");
     /*
     struct song_node *node = NULL;
     node = insert_front(node, c, c);
     lib[i] = node;
     a++;*/
-  }
+  //}
+  struct song_node *head = insert_ordered(NULL, "sober", "big bang");
+  head = insert_ordered(head, "hello", "adele");
+  head = insert_ordered(head, "baby", "justin beiber");
+  print_list(head);
 
-  
+  return 0;
 }
